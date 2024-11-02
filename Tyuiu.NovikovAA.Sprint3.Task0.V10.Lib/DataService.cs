a698bd584865d46d6c2a4fb8c5161bf4cc05e3de
Novikov Alexander <Tyuiu.NovikovAA1@outlook.com>
2024-11-02 05:29:05 +0500

    tsasd

--- Tyuiu.NovikovAA.Sprint3.Task0.V10.Lib/DataService.cs
+++ Tyuiu.NovikovAA.Sprint3.Task0.V10.Lib/DataService.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.NovikAA.Sprint3.Task0.V10.Lib
             double result = 0;
             for (int i = startValue; i < stopValue; i++)
             {
-                result += Math.Pow(300 / (i + Math.Pow(value, i)),i);
+                result += Math.Pow(300 / (i + Math.Pow(value, i)), i);
             }
             return result;
         }
