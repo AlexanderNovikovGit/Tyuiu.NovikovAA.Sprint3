7625e16dc56db732b86a1d4c8be2d77ff0e2d032
Novikov Alexander <Tyuiu.NovikovAA1@outlook.com>
2024-12-13 05:58:03 +0500

    task 0 fix

--- Tyuiu.NovikovAA.Sprint3.Task0.V10.Lib/DataService.cs
+++ Tyuiu.NovikovAA.Sprint3.Task0.V10.Lib/DataService.cs
@@ -7,10 +7,10 @@ namespace Tyuiu.NovikAA.Sprint3.Task0.V10.Lib
     {
         public double GetMultiplySeries(int value, int startValue, int stopValue)
         {
-            double result = 0;
+            double result = 1;
             for (int i = startValue; i < stopValue; i++)
             {
-                result += Math.Pow(300 / (i + Math.Pow(value, i)), i);
+                result *= Math.Pow(300 / (i + Math.Pow(value, i)), i);
             }
             return result;
         }
